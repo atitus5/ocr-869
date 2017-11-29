13273b9c1ac8fc151d4c136ebb45aecb4389da03
atitus5 <drewtitus59@gmail.com>
2017-11-29 14:22:09 -0500

    Added KJV dataset with bigram matrix comp

--- README.txt
+++ README.txt
@@ -6,4 +6,8 @@ MIT 6.869 Advances in Computer Vision -- Final Project
 Fall 2017
 
 Dependencies:
+Python 3.6.2+
+matplotlib
 nltk; uses 'gutenberg' corpus (install with nltk.download())
+numpy
+seaborn
---
+++ utils/kjv_text.py
@@ -0,0 +1,92 @@
+import time
+
+import matplotlib.pyplot as plt
+from nltk.corpus import gutenberg
+from nltk import ngrams
+import numpy as np
+import seaborn as sns; sns.set()
+
+class KJVTextDataset(object):
+    def __init__(self):
+        super(KJVTextDataset, self).__init__()
+
+        # Ignore newlines
+        self.full_text = gutenberg.raw('bible-kjv.txt').replace('\n', '')
+        self.words = gutenberg.words('bible-kjv.txt')
+
+        # Determine counts of unique chars in text, plus a mapping of char->int (for bigram matrix)
+        self.char_counts = dict()
+        self.char_to_int = dict()
+        for char in self.full_text:
+            if char in self.char_counts:
+                self.char_counts[char] += 1
+            else:
+                self.char_counts[char] = 1
+
+            if char not in self.char_to_int:
+                self.char_to_int[char] = len(self.char_to_int) 
+
+        # Don't compute bigrams in initialization -- do on-demand
+        self.char_bigrams = None    # |unique chars| x |unique chars|
+
+    def unique_chars(self):
+        return len(self.char_counts)
+
+    def char_count(self, char):
+        if char in self.char_counts:
+            return self.char_counts[char]
+        else:
+            return 0
+
+    def char_bigram_score(self, char_1, char_2):
+        if self.char_bigrams is None:
+            # Need to compute these first!
+            self._compute_char_bigrams()
+        
+        return self.char_bigrams[self.char_to_int[char_1],
+                                 self.char_to_int[char_2]]
+
+    def char_bigram_matrix(self):
+        if self.char_bigrams is None:
+            # Need to compute these first!
+            self._compute_char_bigrams()
+        
+        return self.char_bigrams
+
+    def char_bigram_heatmap(self):
+        if self.char_bigrams is None:
+            # Need to compute these first!
+            self._compute_char_bigrams()
+        
+        # Visualize the char bigram matrix as a heatmap
+        # Use log probabilities due to large spread from low to high
+        ax = sns.heatmap(np.log(self.char_bigrams),
+                         cmap="jet",
+                         xticklabels=sorted(self.char_to_int.keys()),
+                         yticklabels=sorted(self.char_to_int.keys()))
+        ax.set_title("Char bigram log-probabilities")
+        plt.xlabel("Character 1")
+        plt.ylabel("Character 2")
+        plt.show()
+
+    def _compute_char_bigrams(self):
+        print("Recomputing character bigrams...", flush=True)
+        start_t = time.time()
+
+        all_char_bigrams = ngrams(list(self.full_text), 2)
+
+        # First compute raw counts
+        # Uses eps instead of zero so log prob viz works
+        self.char_bigrams = np.ones((self.unique_chars(), self.unique_chars())) * np.finfo(float).eps
+        total_bigrams = 0
+        for char_bigram in all_char_bigrams:
+            char_1, char_2 = char_bigram[0:2]
+            self.char_bigrams[self.char_to_int[char_1],
+                              self.char_to_int[char_2]] += 1
+            total_bigrams += 1
+
+        # Now, normalize into probabilities
+        self.char_bigrams /= float(total_bigrams)
+
+        end_t = time.time()
+        print("Done recomputing character bigrams (%.3f seconds)." % (end_t - start_t), flush=True)
