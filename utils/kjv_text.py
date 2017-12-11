c84db8ae7a9fc4e482fbefcf847b51209788c659
atitus5 <drewtitus59@gmail.com>
2017-12-10 21:18:13 -0500

    Fixed train/val ranges

--- utils/kjv_text.py
+++ utils/kjv_text.py
@@ -107,7 +107,6 @@ class KJVTextDataset(object):
         return self.one_hot_matrix
 
     # Define our split as 90% train, 10% validation
-    # [T T V T T T T E T T, "", ..., ""]
 
     # Genesis subset: first 200 images
     def dataset_indices(self, dataset, chars_per_line, lines_per_img):
@@ -120,17 +119,12 @@ class KJVTextDataset(object):
             sys.exit(1)
 
     def train_indices(self, chars_per_line, lines_per_img):
-        '''
         full_range = list(range(len(self.image_text(chars_per_line, lines_per_img))))
-        index_set = set(full_range) - set(self.eval_indices(chars_per_line, lines_per_img))
-        '''
-        index_set = set(list(range(200))) - set(self.val_indices(chars_per_line, lines_per_img))
-        return sorted(index_set)
+        return full_range[0:180]
     
     def val_indices(self, chars_per_line, lines_per_img):
         full_range = list(range(len(self.image_text(chars_per_line, lines_per_img))))
-        # return full_range[9:10:] 
-        return full_range[9:10:200] 
+        return full_range[180:200] 
 
     def image_text(self, chars_per_line, lines_per_img):
         num_lines = int(math.ceil(len(self.full_text) / float(chars_per_line)))
